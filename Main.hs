b43d1a1ce233a98e91436fc55360abf3c79a2ab4
Ondra Nekola <ondra@nekola.cz>
2015-03-29 16:24:23 +0200

    moved computing 'simulation' from main into simulation module

--- Main.hs
+++ Main.hs
@@ -11,20 +11,7 @@ import Control.Concurrent
 import System.Environment
 import System.Directory
 
-signal :: [Int] -> [(Int,Double)]
-signal xs = [ (round x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- map fromIntegral xs ]
-
-signal2 xs = [ (round x,(cos (x*3.14159/45) + 1) / 2 * (cos (x*3.14159/5))) | x <-  map fromIntegral xs ]
-
-computeSimulation :: AnalysisParameters -> [(String, [(Int, Double)])]
-computeSimulation params = [("t1", signal [0,7..400]), ("t2", signal2 [0,7..400])]
-
-data AnalysisParameters = AnalysisParameters {
-    multipleRuns :: Bool,
-    separatedGenerations :: Bool,
-    populationSize :: Int
-    }
-
+import Simulation
 
 data AnalysisParametersFields = AnalysisParametersFields {
     multipleRunsField :: CheckButton,
---
+++ src/Simulation.hs
@@ -0,0 +1,20 @@
+module Simulation(computeSimulation, AnalysisParameters(AnalysisParameters)) where
+
+import Data.Random
+
+import Evolution
+
+
+data AnalysisParameters = AnalysisParameters {
+    multipleRuns :: Bool,
+    separatedGenerations :: Bool,
+    populationSize :: Int
+    }
+
+signal :: [Int] -> [(Int,Double)]
+signal xs = [ (round x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- map fromIntegral xs ]
+
+signal2 xs = [ (round x,(cos (x*3.14159/45) + 1) / 2 * (cos (x*3.14159/5))) | x <-  map fromIntegral xs ]
+
+computeSimulation :: AnalysisParameters -> [(String, [(Int, Double)])]
+computeSimulation params = [("t1", signal [0,7..400]), ("t2", signal2 [0,7..400])]
