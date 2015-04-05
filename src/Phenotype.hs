e5cac15e41fb7c33545da2e322df672afc52c8b9
Ondra Nekola <ondra@nekola.cz>
2015-04-05 09:14:03 +0200

    utility function, that extracts values from Phenotype

--- src/Phenotype.hs
+++ src/Phenotype.hs
@@ -1,6 +1,9 @@
-module Phenotype(Phenotype(Phenotype), distance) where
+module Phenotype(Phenotype(Phenotype), distance, phenotypeToVector) where
 
 data Phenotype = Phenotype [Double] deriving (Eq, Show)
+
+phenotypeToVector :: Phenotype -> [Double]
+phenotypeToVector (Phenotype xs) = xs
 
 distance :: Phenotype -> Phenotype -> Double
 distance (Phenotype p1) (Phenotype p2) = sqrt $ sum $ map (^2) $ zipWith (-) p1 p2
