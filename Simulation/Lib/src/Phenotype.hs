f8b1ad227343b42895de7b1f8bb4165c904b4749
Ondra Nekola <ondra@nekola.cz>
2016-05-29 15:52:07 +0200

    minor: refactor distance to make clear, it's euclidean one

--- Simulation/Lib/src/Phenotype.hs
+++ Simulation/Lib/src/Phenotype.hs
@@ -6,4 +6,10 @@ phenotypeToVector :: Phenotype -> [Double]
 phenotypeToVector (Phenotype xs) = xs
 
 distance :: Phenotype -> Phenotype -> Double
-distance (Phenotype p1) (Phenotype p2) = sqrt $ sum $ map (\ x -> x * x) $ zipWith (-) p1 p2
+distance (Phenotype p1) (Phenotype p2) = euclideanDistance p1 p2
+  where
+    sqr :: Double -> Double
+    sqr x = x * x
+
+    euclideanDistance :: [Double] -> [Double] -> Double
+    euclideanDistance a b = sqrt $ sum $ map sqr $ zipWith (-) a b
