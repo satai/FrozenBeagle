bd58427f85533a9caa5f69fd51c67498110aa775
Ondra Nekola <ondra@nekola.cz>
2017-02-25 20:17:23 +0100

    CLI - run sim and exit

--- Simulation/CLI/src/Main.hs
+++ Simulation/CLI/src/Main.hs
@@ -1,2 +1,27 @@
+import Simulation
+
+main :: IO ()
 main = do
-    print "TODO"
+    putStr "["
+    mapM_ runSimWithSeed [1..5]
+    putStr "]"
+
+runSimWithSeed :: Int -> IO ()
+runSimWithSeed seed = do
+    parameters <- extractParams
+    let simResults = computeSimulation parameters
+    print simResults
+
+extractParams :: IO AnalysisParameters
+extractParams =
+    return AnalysisParameters {
+           separatedGenerations = False
+         , hardSelectionTreshold =  0.0
+         , populationSize =  300
+         , optimumChange = []
+         , maxAge = 64
+         , countOfBases = 50
+         , countOfPleiotropicRules = 0
+         , countOfEpistaticRules = 0
+         , countOfComplicatedRules = 0
+      }
