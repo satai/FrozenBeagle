ea787bc16c00eb4a851c267b84f40b1407b8189c
Ondra Nekola <ondra@nekola.cz>
2017-04-02 15:19:03 +0200

    check that eauclidean distance inputs are of same length

--- Simulation/Lib/src/Phenotype.hs
+++ Simulation/Lib/src/Phenotype.hs
@@ -23,7 +23,12 @@ distance (Phenotype p1) (Phenotype p2) = euclideanDistance p1 p2
     sqr x = x * x
 
     euclideanDistance :: [Double] -> [Double] -> Double
-    euclideanDistance a b = sqrt $ sum $ map sqr $ zipWith (-) a b
+    euclideanDistance a b =
+        if length a == length b
+        then
+            sqrt $ sum $ map sqr $ zipWith (-) a b
+        else
+            error $ "cant't measure distance of two vectors of different lengths " ++ show a ++ " vs " ++ show b
 
 fitness :: Phenotype -> Phenotype -> Double
 fitness optimum individual = log $ 25.0 / (individual `distance` optimum + 0.001)
